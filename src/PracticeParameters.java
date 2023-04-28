3fa73e0718da099e0e096b51ec27bcf053a0ac0b
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2023-04-28 18:13:07 -0400

    removed solution

--- src/PracticeParameters.java
+++ src/PracticeParameters.java
@@ -1,25 +1,18 @@
 public class PracticeParameters {
-    public static String one_parameter(String name) {
-        return "Hello, my name is " + name + ".";
-//        return null;
+    public static String one_parameter() {
+        return null;
     }
 
-    public static String two_parameters(String name, String event) {
-        return "My name is " + name + ", and I work as a full-time " + event + ".";
-//        return null;
+    public static String two_parameters() {
+        return null;
     }
 
-    public static String one_parameter_if_exists(String name ) {
-        if (name == null) {
-            return "Welcome, Everyone!";
-        } else {
-            return "Welcome, " + name + "!";
-        }
-//        return null;
+    public static String one_parameter_if_exists() {
+        return null;
     }
 
-    public static String three_parameters_different_data_types(String school, int year, String name) {
-        return "My name is " + name + " and I graduated from " + school + " in "+ year + ".";
+    public static String three_parameters_different_data_types() {
+        return null;
     }
 
 }
